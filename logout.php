48cec80f044e153d49618bf200d9ec07f4667303
Ozuna <33678911+OzunaEdd@users.noreply.github.com>
2019-09-05 17:58:13 -0400

    Update logout.php

--- logout.php
+++ logout.php
@@ -10,7 +10,7 @@ session_start();
   <body>
     <?php
     $userName = $_SESSION['username'];
-    $dbc = mysqli_connect("localhost", "root", "", "csc350") or die(mysql_error()); //connect to server
+    $dbc = mysqli_connect("m7nj9dclezfq7ax1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "ew33q2sqesuu10bz", "u7uhn2d28ofq7vn8", "c0a4ao6srswkd8yd") or die(mysql_error()); //connect to server
     $logout = "INSERT INTO activity(user,type,time) VALUES('$userName','LOG OUT',NOW())";
     mysqli_query($dbc,$logout);
     header("Location: index.php");
